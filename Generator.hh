a34656ed075768803fb70f700cb3c4350dbdeb9a
IoanBalteanu <81511768+IoanBalteanu@users.noreply.github.com>
2021-03-29 21:37:25 +0200

    added skeleton of main

--- Generator.hh
+++ Generator.hh
@@ -6,16 +6,18 @@
 
 using namespace std;
 
+typedef vector<vector<int>> PROBLEM;
+
 class Generator
 {
     private:
         
         Generator(){}
         
-        static void getTable(vector<vector<int>>& table, int n);
+        static void getTable(PROBLEM& table, int n);
         
-        static void shuffleTable(vector<vector<int>>& table, int n);
+        static void shuffleTable(PROBLEM& table, int n);
         
     public:
-        static vector<vector<vector<int>>> getProblems(int nProbs, int nRms);
+        static vector<PROBLEM> getProblems(int nProbs, int nRms);
 };
--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 
 using namespace std;
 
-void printTables(vector<vector<vector<int>>>& table, int probs, int habs) {
+void printTables(vector<PROBLEM>& table, int probs, int habs) {
     
     habs *= 2;
+    
+    cout << "### SET WITH " << probs << " PROBLEMS OF SIZE " <<  habs << " ###" <<  endl; 
     
     for (int p = 0 ; p < probs ; ++p){
         for (int i = 0; i < habs; ++i) {
@@ -18,19 +20,50 @@ void printTables(vector<vector<vector<int>>>& table, int probs, int habs) {
         
         if(p < probs-1)cout << endl;
     }
+    
+    cout << endl;
 }
 
 int main()
 {
-    int nProbs = 0, nRms = 0;
-    cout << "Input number of problems to be generated: ";
-    cin >> nProbs;
-    cout << "Input number of available rooms: ";
-    cin >> nRms;
+    int generationType = -1;
+    cout << "0: One set of problems." << endl <<  "1: N sets of problems with size within [R,L] where |N| = L - R" << endl << "#R must be greater or equal to 2#" << endl <<"Choose generation type: ";
     
-    vector<vector<vector<int>>> Problems = Generator::getProblems(nProbs,nRms);
+    bool correctGT = false;
     
-    printTables(Problems, nProbs, nRms);
+    while(!correctGT)
+    {
+        cin >> generationType;
+        
+        if(generationType == 0)
+        {
+            correctGT = true;
+            int nProbs = 0, nRms = 0;
+            cout << "Input number of problems to be generated: ";
+            cin >> nProbs;
+            cout << "Input number of available rooms: ";
+            cin >> nRms;
+            
+            vector<PROBLEM> Problems = Generator::getProblems(nProbs,nRms);
+            
+            printTables(Problems, nProbs, nRms);
+        }
+        else if (generationType == 1)
+        {
+            correctGT = true;
+            int nProbs = 0, nRms = 0;
+            cout << "Input number of problems to be generated per set: ";
+            cin >> nProbs;
+            
+            cout << "Input minimum size of a problem (L): ";
+            
+            
+        }
+        else
+        {
+            cout << "Incorrect generation type, try again: ";
+        }
+    }
     
     return 0;
 }
